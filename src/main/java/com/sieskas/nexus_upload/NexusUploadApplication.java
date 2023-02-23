21ce28f8bde82ace1f1dcc4200dc015ae8151b8e
rogodard <sieskas@gmail.com>
2023-02-23 04:25:24 -0500

    init commit

---
+++ src/main/java/com/sieskas/nexus_upload/NexusUploadApplication.java
@@ -0,0 +1,13 @@
+package com.sieskas.nexus_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusUploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sieskas/nexus_upload/NexusUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sieskas.nexus_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
